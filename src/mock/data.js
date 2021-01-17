5a307dddb885a32a2198c93cbc682a582baed33c
Ennio Maldonado <techno1731@gmail.com>
2021-01-17 10:13:26 +0100

    Updated Resume Link

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: "Ennio is a curiosity driven and taco's eating analytical data driven engineer with strong problem solving skills. Originally from mexico and currently living in sunny Barcelona, he enjoys technology, science, and believes there must be life beyond earth.",
   paragraphTwo: 'Ennio has over 5 years of experience working in global projects, for the last two years he has helped improve products and services for customers by using advanced problem solving, based on statistical techniques, leveraging big-data analytical tools, creating and maintaining models, and onboarding compelling new data sets from production environments.',
   paragraphThree: 'Competencies: Problem Solving, Data Science, Machine Learning, Cloud Computing, Spark, Python/Scala/R, Git, SQL, Data and Model Versioning, MLops.',
-  resume: 'https://drive.google.com/file/d/1JpzLmSvEclyReW_eL_ds-8jblBJ7hvzD/view?usp=sharing', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1Y03r9WunfDNQb5kLP0b35PxwM2_hJU2r/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
